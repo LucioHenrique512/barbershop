9c129a56f9e8f07582772403a91e4310d088353b
Lucio Henrique <lucions96@gmail.com>
2022-09-11 13:44:13 -0300

    setup swagger

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller('company')
-export class CompanyController {}
+@ApiTags('company')
+@Controller('v1/company')
+export class CompanyController {
+  @ApiOperation({ summary: 'Get list of companies' })
+  @Get()
+  async getAll() {
+    return [];
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { execSync } from 'child_process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/default');
+
+  const config = new DocumentBuilder()
+    .setTitle('Barbershop microservice')
+    .setDescription('The barbershop microservices.')
+    .setVersion('0.0.1')
+    .addTag('company')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
